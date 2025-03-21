4052332c28c07717497ea45b096c719411aec193
Sendou <janan.karur@gmail.com>
2025-03-21 15:02:16 +0530

    proof generator

---
+++ circuits/build.sh
@@ -0,0 +1,32 @@
+rm -rf target
+
+echo "Compiling contract: "
+nargo execute
+
+echo "Compiling circuit..."
+if ! nargo compile; then
+    echo "Compilation failed. Exiting..."
+    exit 1
+fi
+
+echo "Gate count: "
+bb gates -b target/zk_platoon.json | jq  '.functions[0].circuit_size'
+
+
+mkdir -p "../simulator/assets/zk_platoon"
+
+echo "Copying circuit.json to simulator/assets/zk_platoon..."
+cp target/zk_platoon.json "../simulator/assets/zk_platoon/circuit.json"
+
+
+echo "Generating Key: "
+bb write_vk_ultra_keccak_honk -b ./target/zk_platoon.json
+
+echo "Generating Contract: "
+bb contract_ultra_honk
+
+echo "Generating vkey.json to simulator/assets/zk_platoon..."
+node -e "const fs = require('fs'); fs.writeFileSync('../simulator/assets/zk_platoon/circuit-vkey.json', JSON.stringify(Array.from(Uint8Array.from(fs.readFileSync('./target/vk')))));"
+
+
+echo "Done!"
--- simulator/src/utils/zk_platoon.ts
+++ simulator/src/utils/zk_platoon.ts
@@ -1,6 +1,11 @@
 import React from "react";
 import { InputMap, type CompiledCircuit } from "@noir-lang/noir_js";
 import { initProver } from "./lazy-modules";
+import initNoirC from "@noir-lang/noirc_abi";
+import initACVM from "@noir-lang/acvm_js";
+import acvm from "@noir-lang/acvm_js/web/acvm_js_bg.wasm?url";
+import noirc from "@noir-lang/noirc_abi/web/noirc_abi_wasm_bg.wasm?url";
+await Promise.all([initACVM(fetch(acvm)), initNoirC(fetch(noirc))]);
 
 export const zk_platoon = {
   generateProof: async () => {
